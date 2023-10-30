b157f9b433a6a90bd36b69fdb13994dafb744222
thpndx32 <62064823+thpndx32@users.noreply.github.com>
2023-10-29 20:10:34 -0500

    añadido de perfil de talento

--- frontend/src/Views/PerfilTalento.jsx
+++ frontend/src/Views/PerfilTalento.jsx
@@ -1,8 +1,17 @@
-import "../Styles/css-styles/views.css"
+import { ContenidoPerfil } from "../Components/PerfilTalento/ContenidoPerfil";
+import "../Styles/css-styles/views.css";
+import "../Styles/css-styles/perfilTalento.css";
+import { Datos } from "../Components/PerfilTalento/Datos";
 export const PerfilTalento = () => {
     return (
-        <div className="bodyContent">
-            PERFIL TALENTO
+        <div className="perfil">
+            <div className="perfilTalento">
+            <ContenidoPerfil/>
+            </div>
         </div>
     )
 }
+/*
+    en lugar de crear una pagina web por cada uno crear un componente que s puede crear
+    por cada perfil sacandolo desde la base de datos
+*/ 
